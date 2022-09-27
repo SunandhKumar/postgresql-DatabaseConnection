39cc1f86dbc394270a74d6faf57ee9e2cc1fd6b2
SunandhKumar <89825655+SunandhKumar@users.noreply.github.com>
2022-09-27 17:34:17 +0530

    Add files via upload

---
+++ pgsqlDatabaseConnection/src/pgsqlDatabaseConnection/PsqlConnector.java
@@ -0,0 +1,75 @@
+//$Id$
+package pgsqlDatabaseConnection;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class PsqlConnector {
+		
+	    private final static String url = "jdbc:postgresql://localhost:5432/demo_db";
+	    private final static String user = "postgres";
+	    private final static String password = "postgres";
+
+		@SuppressWarnings("resource")
+		public static void main(String args[]){
+	        //String SQL = "INSERT INTO student VALUES(?,?,?)";
+			String SQL = "INSERT INTO users (username,email,age,firstname,lastname,location,gender,phoneno) VALUES(?,?,?,?,?,?,?,?)";
+	        try (Connection conn = connect();
+	            PreparedStatement pstmt = conn.prepareStatement(SQL,Statement.RETURN_GENERATED_KEYS)) {
+//	        	Scanner sc=new Scanner(System.in);
+//	        	System.out.println("Enter sid:");
+//	        	int x=sc.nextInt();
+//	        	String a,b;
+//	        	System.out.println("Enter sname:");
+//	        	a=sc.next();
+//	        	System.out.println("Enter dept:");
+//	        	b=sc.next();
+//	        	System.out.println(x+" "+a+" "+b);
+//	            pstmt.setInt(1, x);
+//	            pstmt.setString(2, a);
+//	            pstmt.setString(3, b);
+	        	
+	        	pstmt.setString(1, "spidey");
+	            pstmt.setString(2, "marvel@gmail.com");
+	            pstmt.setInt(3, 20);
+	            pstmt.setString(4, "peter");
+	            pstmt.setString(5, "parker");
+	            pstmt.setString(6, "New york");
+	            pstmt.setString(7, "male");
+	            pstmt.setString(8, "9934736726");
+
+	            int affectedRows = pstmt.executeUpdate();
+	            System.out.println(affectedRows);
+	            delete();
+	        } 
+	        catch (SQLException ex) {
+	            System.out.println(ex.getMessage());
+	        }
+	    }
+
+
+		public static Connection connect() throws SQLException {
+		    return DriverManager.getConnection(url, user, password);
+		}
+		
+		public static int delete() {
+			 String SQL = "DELETE FROM student WHERE sid= ? ";
+			 try (Connection conn = connect();
+		            PreparedStatement pstmt = conn.prepareStatement(SQL,Statement.RETURN_GENERATED_KEYS)) {
+		        	@SuppressWarnings("resource")
+					Scanner sc=new Scanner(System.in);
+		        	System.out.println("Enter sid:");
+		        	int x=sc.nextInt();
+		        	System.out.println(x);
+		            pstmt.setInt(1, x);
+
+		            int affectedRows = pstmt.executeUpdate();
+		            System.out.println(affectedRows);
+		     }
+			 catch (SQLException ex) {
+		            System.out.println(ex.getMessage());
+		     }
+			 return 0;
+		}
+	
+}
