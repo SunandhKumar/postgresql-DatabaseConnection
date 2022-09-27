6cb817ea1afe76cefc7800c92c9df04c85d164fd
SunandhKumar <89825655+SunandhKumar@users.noreply.github.com>
2022-09-27 17:42:54 +0530

    Update PsqlConnector.java

--- pgsqlDatabaseConnection/src/pgsqlDatabaseConnection/PsqlConnector.java
+++ pgsqlDatabaseConnection/src/pgsqlDatabaseConnection/PsqlConnector.java
@@ -10,26 +10,12 @@ public class PsqlConnector {
 	    private final static String user = "postgres";
 	    private final static String password = "postgres";
 
-		@SuppressWarnings("resource")
-		public static void main(String args[]){
-	        //String SQL = "INSERT INTO student VALUES(?,?,?)";
-			String SQL = "INSERT INTO users (username,email,age,firstname,lastname,location,gender,phoneno) VALUES(?,?,?,?,?,?,?,?)";
-	        try (Connection conn = connect();
-	            PreparedStatement pstmt = conn.prepareStatement(SQL,Statement.RETURN_GENERATED_KEYS)) {
-//	        	Scanner sc=new Scanner(System.in);
-//	        	System.out.println("Enter sid:");
-//	        	int x=sc.nextInt();
-//	        	String a,b;
-//	        	System.out.println("Enter sname:");
-//	        	a=sc.next();
-//	        	System.out.println("Enter dept:");
-//	        	b=sc.next();
-//	        	System.out.println(x+" "+a+" "+b);
-//	            pstmt.setInt(1, x);
-//	            pstmt.setString(2, a);
-//	            pstmt.setString(3, b);
-	        	
-	        	pstmt.setString(1, "spidey");
+	    @SuppressWarnings("resource")
+	    public static void main(String args[]){
+		String SQL = "INSERT INTO users (username,email,age,firstname,lastname,location,gender,phoneno) VALUES(?,?,?,?,?,?,?,?)";
+	        try (Connection conn = connect(); PreparedStatement pstmt = conn.prepareStatement(SQL,Statement.RETURN_GENERATED_KEYS)) {
+
+	            pstmt.setString(1, "spidey");
 	            pstmt.setString(2, "marvel@gmail.com");
 	            pstmt.setInt(3, 20);
 	            pstmt.setString(4, "peter");
@@ -41,35 +27,34 @@ public class PsqlConnector {
 	            int affectedRows = pstmt.executeUpdate();
 	            System.out.println(affectedRows);
 	            delete();
+			
 	        } 
 	        catch (SQLException ex) {
 	            System.out.println(ex.getMessage());
 	        }
 	    }
 
-
-		public static Connection connect() throws SQLException {
-		    return DriverManager.getConnection(url, user, password);
-		}
+	  public static Connection connect() throws SQLException {
+		 return DriverManager.getConnection(url, user, password);
+	  }
 		
-		public static int delete() {
-			 String SQL = "DELETE FROM student WHERE sid= ? ";
-			 try (Connection conn = connect();
-		            PreparedStatement pstmt = conn.prepareStatement(SQL,Statement.RETURN_GENERATED_KEYS)) {
+	  public static int delete() {
+	         String SQL = "DELETE FROM users WHERE username= ? ";
+	   	 try (Connection conn = connect();  PreparedStatement pstmt = conn.prepareStatement(SQL,Statement.RETURN_GENERATED_KEYS)) {
 		        	@SuppressWarnings("resource")
-					Scanner sc=new Scanner(System.in);
-		        	System.out.println("Enter sid:");
-		        	int x=sc.nextInt();
+				Scanner sc=new Scanner(System.in);
+		        	System.out.println("Enter usename:");
+		        	String x=sc.next();
 		        	System.out.println(x);
-		            pstmt.setInt(1, x);
+		            	pstmt.setString(1, x);
 
-		            int affectedRows = pstmt.executeUpdate();
-		            System.out.println(affectedRows);
-		     }
-			 catch (SQLException ex) {
+		            	int affectedRows = pstmt.executeUpdate();
+		           	System.out.println(affectedRows);
+		 }
+		 catch (SQLException ex) {
 		            System.out.println(ex.getMessage());
-		     }
-			 return 0;
-		}
+		 }
+		return 0;
+	  }
 	
 }
